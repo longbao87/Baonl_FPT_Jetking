f3eeec6333fed5f25f5d14247eff712728124f61
root <root@localhost.localdomain>
2022-05-01 01:16:57 +0700

    upload Script Bai1-HelloWorld.sh

---
+++ Bai1-HelloWorld.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Đây là câu lệnh thực hiện xoá thông tin đang thể hiện trên màn hình terminal.
+clear
+
+#Đây là dòng lệnh echo hỗ trợ in câu "Hello World" ra màn hình
+echo "Hello World"
+
+exit 0
